69367ac20f9cdeb7cc39e91a21ab6a161750aa88
Annette Maza <nettymaza@gmail.com>
2017-10-04 21:22:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     result = ''
     params[:number].to_i.times do
-      result += params[:string].to_s
+      result += params[:string]
     end
     result
   end
